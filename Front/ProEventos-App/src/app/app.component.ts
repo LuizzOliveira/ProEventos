4b1af8cc11d9ae46093877b408ed72970033f22e
Luiz <lhso1991@gmail.com>
2025-03-23 01:12:32 -0400

    ioio

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
-import { User } from './models/identity/User';
-import { AccountService } from './services/account.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  constructor(public accountService: AccountService) {}
-
-  ngOnInit(): void {
-    this.setCurrentUser();
-  }
-
-  setCurrentUser(): void {
-    let user: User;
-
-    if (localStorage.getItem('user'))
-      user = JSON.parse(localStorage.getItem('user') ?? '{}');
-    else
-      user = null
-
-    if (user)
-      this.accountService.setCurrentUser(user);
-  }
-
+  title = 'ProEventos-App';
 }
